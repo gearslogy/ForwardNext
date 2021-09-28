04854fa9aaf449f0c30cc776e696649833a25194
liuyangping <liuyangping207@qq.com>
2021-09-28 11:05:10 +0800

    update

--- PCG_Detail.hpp
+++ PCG_Detail.hpp
@@ -18,33 +18,35 @@
 #include <concepts>
 
 
-//! @std::string name, @std::any valueHandle, @ati is only register for meta information
-using PCG_AttributeValueType = std::tuple<std::string , std::any, PCG_AttributeTypeInfo >;
+//! @std::string name, @std::any valueHandle, @ati is only register for meta information, this handle attrib
+using PCG_AttribValueType = std::tuple<std::string , std::any, PCG_AttributeTypeInfo >;
 
+
+// attrib value handle, it's a container
 class PCG_Detail{
 private:
-    std::vector<PCG_AttributeValueType> dataHandle;
+    std::vector<PCG_AttribValueType> dataHandle;
 public:
     PCG_Detail() = default;
     template<typename T>
     PCG_Detail(std::string_view name, const T && value , PCG_AttributeTypeInfo info){
-        auto att = PCG_Detail::createAttribute(name, std::forward<T>(value), info);
+        auto att = PCG_Detail::createAttrib(name, std::forward<T>(value), info);
         dataHandle.emplace_back(std::move(att));
     }
 
     template<typename T>
     PCG_Detail(std::string_view name, T && value , PCG_AttributeTypeInfo info){
-        auto att = PCG_Detail::createAttribute(name, std::forward<T>(value), info);
+        auto att = PCG_Detail::createAttrib(name, std::forward<T>(value), info);
         dataHandle.emplace_back(std::move(att));
     }
 
     template<typename T>
-    static auto createAttribute(std::string_view name, T && value, PCG_AttributeTypeInfo info){
+    static auto createAttrib(std::string_view name, T && value, PCG_AttributeTypeInfo info){
         return std::make_tuple(std::string(name), std::make_any<T>(std::forward<T>(value)), info );
     }
 
-    void appendAttrib( PCG_AttributeValueType && attrib){
-        dataHandle.emplace_back(std::forward<PCG_AttributeValueType>(attrib ));
+    void appendAttrib(PCG_AttribValueType && attrib){
+        dataHandle.emplace_back(std::forward<PCG_AttribValueType>(attrib ));
     }
 
     void removeAttrib(std::string_view queryName){
@@ -101,7 +103,7 @@ public:
         });
     }
     // overload method for attrib that was existed
-    bool hasAttrib(const PCG_AttributeValueType & rh){
+    bool hasAttrib(const PCG_AttribValueType & rh){
         auto name = std::get<std::string>(rh);
         return hasAttrib(name);
     }
@@ -140,6 +142,18 @@ public:
         return getAttribValueOpt<T>(queryName)->get();
     }
 
+    // get value by index
+    template<class T>
+    const T & getAttribValue(size_t index) const {
+        auto &handle = dataHandle[index];
+        auto &var = std::get<std::any>(handle);
+        return std::any_cast<const T&>(var);
+    }
+    template<class T>
+    T & getAttribValue(size_t index){
+        return const_cast<T&>(std::as_const(*this).getAttribValue<T>(index));
+    }
+
 };
 
 // print all attribute names
@@ -172,5 +186,45 @@ template<typename T>
 inline void PCG_SetAttribValue(PCG_Detail &rh, std::string_view name,  T && value){
     rh.setAttribValue(name, std::forward<T>(value) );
 }
-
+// create attribute, if value is custom user data, the ati of PCG_AttributeValueType is invalid,
+// you can change the type  std::get<PCG_AttributeTypeInfo>(ret) = PCG_AttributeTypeInfo::P_ATI_INT;
+template<typename T>
+inline auto PCG_CreateAttrib(std::string_view name, T && value){
+    using value_type = std::remove_cvref_t<T>;
+    PCG_AttribValueType ret = PCG_Detail::createAttrib(name, static_cast<value_type>(value),
+                                                       PCG_AttributeTypeInfo::P_ATI_INVALID);
+    if constexpr(std::is_same_v<value_type, int>){
+        std::get<PCG_AttributeTypeInfo>(ret) = PCG_AttributeTypeInfo::P_ATI_INT;
+    }
+    else if constexpr(std::is_same_v<value_type, float>){
+        std::get<PCG_AttributeTypeInfo>(ret) = PCG_AttributeTypeInfo::P_ATI_FLT;
+    }
+    else if constexpr(std::is_same_v<value_type, double>){
+        std::get<PCG_AttributeTypeInfo>(ret) = PCG_AttributeTypeInfo::P_ATI_DOUBLE;
+    }
+    else if constexpr(std::is_same_v<value_type, std::string>){
+        std::get<PCG_AttributeTypeInfo>(ret) = PCG_AttributeTypeInfo::P_ATI_STR;
+    }
+    else if constexpr(std::is_same_v<value_type, glm::vec2>){
+        std::get<PCG_AttributeTypeInfo>(ret) = PCG_AttributeTypeInfo::P_ATI_VEC2;
+    }
+    else if constexpr(std::is_same_v<value_type, glm::vec3>){
+        std::get<PCG_AttributeTypeInfo>(ret) = PCG_AttributeTypeInfo::P_ATI_VEC3;
+    }
+    else if constexpr(std::is_same_v<value_type, glm::vec4>){
+        std::get<PCG_AttributeTypeInfo>(ret) = PCG_AttributeTypeInfo::P_ATI_VEC4;
+    }
+    else if constexpr(std::is_same_v<value_type, glm::mat2>){
+        std::get<PCG_AttributeTypeInfo>(ret) = PCG_AttributeTypeInfo::P_ATI_MAT22;
+    }
+    else if constexpr(std::is_same_v<value_type, glm::mat3>){
+        std::get<PCG_AttributeTypeInfo>(ret) = PCG_AttributeTypeInfo::P_ATI_MAT33;
+    }
+    else if constexpr(std::is_same_v<value_type, glm::mat4>){
+        std::get<PCG_AttributeTypeInfo>(ret) = PCG_AttributeTypeInfo::P_ATI_MAT44;
+    }
+    else{
+    }
+    return ret;
+}
 #endif //NODE_AND_ITEMS_PCG_DETAIL_HPP
